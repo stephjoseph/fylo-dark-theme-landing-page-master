7f9ef6891cdc675b926b89f480ff633e85162782
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-05 12:00:45 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="z-10 mb-[7.5rem] flex w-full max-w-[26.75rem] flex-col items-center">
+      <Navbar />
+      <Hero />
+    </header>
+  );
 };
 
 export default Header;
