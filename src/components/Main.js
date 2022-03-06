c97a84458413bda0d789a2bced7ba4b19d9a4d48
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-06 11:13:36 +0800

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
 import CardsSection from "./CardsSection";
+import StayProductive from "./StayProductive";
 
 const Main = () => {
   return (
     <main className="z-10 flex w-[21rem] flex-col items-center">
       <CardsSection />
+      <StayProductive />
     </main>
   );
 };
