addc7e512ec1af561c10d6e199b3fdd6eb75e8f8
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-07 15:21:50 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,18 @@ import logo from "../images/logo.svg";
 
 const Navbar = () => {
   return (
-    <nav className="mt-6 mb-12 flex w-[20.688rem] items-center justify-between">
+    <nav className="mt-6 mb-12 flex w-[20.688rem] items-center justify-between xl:mb-[4.688rem] xl:mt-[4.563rem] xl:w-[88.88vw] xl:max-w-[80rem]">
       <a href="#">
-        <img className="h-6 w-[5rem]" src={logo} alt="" />
+        <img
+          className="h-6 w-[5rem] xl:h-[3.25rem] xl:w-[11rem]"
+          src={logo}
+          alt=""
+        />
       </a>
-      <ul className="flex items-center gap-6">
+      <ul className="flex items-center gap-6 xl:gap-[3.625rem]">
         <li>
           <a
-            className="font-['Raleway'] text-[0.75rem] font-normal leading-[0.875rem] tracking-normal text-white"
+            className="font-['Raleway'] text-[0.75rem] font-normal leading-[0.875rem] tracking-normal text-white xl:text-base xl:leading-[1.188rem]"
             href="#"
           >
             Features
@@ -18,7 +22,7 @@ const Navbar = () => {
         </li>
         <li>
           <a
-            className="font-['Raleway'] text-[0.75rem] font-normal leading-[0.875rem] tracking-normal text-white"
+            className="font-['Raleway'] text-[0.75rem] font-normal leading-[0.875rem] tracking-normal text-white xl:text-base xl:leading-[1.188rem]"
             href="#"
           >
             Team
@@ -26,7 +30,7 @@ const Navbar = () => {
         </li>
         <li>
           <a
-            className="font-['Raleway'] text-[0.75rem] font-normal leading-[0.875rem] tracking-normal text-white"
+            className="font-['Raleway'] text-[0.75rem] font-normal leading-[0.875rem] tracking-normal text-white xl:text-base xl:leading-[1.188rem]"
             href="#"
           >
             Sign in
