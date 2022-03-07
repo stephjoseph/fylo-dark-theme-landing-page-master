5b78d43dcb19b037ed662cc597c3277827cb89ef
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-08 00:16:01 +0800

    Update CTA.js

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -1,6 +1,26 @@
 import React from "react";
+import validator from "validator";
 
 const CTA = () => {
+  const [formData, setFormData] = React.useState({
+    email: "",
+    isValid: false,
+  });
+
+  const validateEmail = validator.isEmail(formData.email);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <section className="z-10 flex w-[21rem] flex-col items-center gap-8 rounded-[9px] bg-[#1B2330] px-[1.75rem] pt-10 pb-[2.313rem] shadow-[5px_5px_8px_0px_rgba(0,0,0,0.2543)] xl:w-[53.938rem] xl:gap-[2.375rem] xl:px-[4.813rem] xl:pb-[2.563rem] xl:pt-[2.625rem]">
       <div className="flex w-full flex-col items-center gap-4 text-center xl:w-[42.063rem]">
@@ -13,12 +33,25 @@ const CTA = () => {
           be happy to help you.
         </p>
       </div>
-      <form className="flex w-full flex-col items-center gap-6 xl:flex-row">
+      <form
+        className="relative flex w-full flex-col items-center gap-6 xl:flex-row"
+        onSubmit={handleSubmit}
+      >
         <input
-          className="h-12 w-full rounded-[24px] px-[1.813rem] py-[1.188rem] text-[0.625rem] font-normal leading-[0.875rem] tracking-normal text-black shadow-[1px_1px_3px_0px_rgba(7,4,59,0.157)] placeholder:text-[0.625rem] placeholder:font-normal placeholder:leading-[0.875rem] placeholder:tracking-normal placeholder:text-[#C0C0C0] focus:outline-none focus:outline-[#62E0D9] xl:w-[30rem] xl:text-[0.75rem] xl:leading-4 xl:placeholder:text-[0.75rem] xl:placeholder:leading-4"
+          className="h-12 w-full rounded-[24px] px-[1.813rem] py-[1.188rem] text-[0.625rem] font-normal leading-[0.875rem] tracking-normal text-black shadow-[1px_1px_3px_0px_rgba(7,4,59,0.157)] placeholder:text-[0.625rem] placeholder:font-normal placeholder:leading-[0.875rem] placeholder:tracking-normal placeholder:text-[#C0C0C0] focus:outline-none focus:outline-[#62E0D9] xl:w-[30rem] xl:py-4 xl:px-[2.375rem] xl:text-[0.75rem] xl:leading-4 xl:placeholder:text-[0.75rem] xl:placeholder:leading-4"
           type="email"
           placeholder="email@example.com"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
         />
+
+        {!validateEmail && (
+          <p className="absolute left-[2.375rem] top-[3.313rem] text-[0.625rem] font-bold leading-[0.875rem] tracking-normal text-[#FF4242]">
+            Error, please check your email
+          </p>
+        )}
+
         <button className="flex h-12 w-full items-center justify-center rounded-[24px] bg-gradient-to-br from-[#63E1D9] to-[#34A0CD] font-['Raleway'] text-[0.875rem] font-bold capitalize leading-4 tracking-normal text-white shadow-[2px_3px_6px_0px_rgba(0,0,0,0.1593)] active:from-[#8ADAE3] active:to-[#8ADAE3] xl:w-[12.5rem]">
           Get started for free
         </button>
