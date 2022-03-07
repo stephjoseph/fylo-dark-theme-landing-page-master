b9ad0a30bc9b1f9f6b819d0c8daeee32c8629714
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-07 15:21:44 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Hero from "./Hero";
 
 const Header = () => {
   return (
-    <header className="z-10 mb-[7.5rem] flex w-full max-w-[26.75rem] flex-col items-center">
+    <header className="z-10 mb-[7.5rem] flex w-full max-w-[26.75rem] flex-col items-center xl:mb-[9.875rem] xl:max-w-[90rem]">
       <Navbar />
       <Hero />
     </header>
