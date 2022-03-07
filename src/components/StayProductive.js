785a8de5ea1d2a1433eff23f9be83075e8806deb
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-07 15:46:03 +0800

    Update StayProductive.js

--- src/components/StayProductive.js
+++ src/components/StayProductive.js
@@ -25,20 +25,58 @@ const StayProductive = () => {
           for live collaboration. No email attachments required.
         </p>
         <a
-          className="flex flex-col gap-[0.375rem] text-[0.75rem] font-normal leading-4 tracking-normal text-[#62E0D9] xl:text-base xl:leading-[1.375rem]"
+          className="group flex flex-col gap-[0.375rem] text-[0.75rem] font-normal leading-4 tracking-normal text-[#62E0D9] hover:text-white xl:text-base xl:leading-[1.375rem]"
           href="#"
         >
           <div className="flex items-center gap-2 xl:gap-1">
             <p>See how Fylo works</p>
             <div>
-              <img
-                className="h-[0.75rem] w-[0.75rem] xl:h-4 xl:w-4"
-                src={iconArrow}
-                alt=""
-              />
+              <svg
+                width="16"
+                height="16"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+              >
+                <defs>
+                  <circle id="b" cx="6" cy="6" r="6" />
+                  <filter
+                    x="-25%"
+                    y="-25%"
+                    width="150%"
+                    height="150%"
+                    filterUnits="objectBoundingBox"
+                    id="a"
+                  >
+                    <feOffset in="SourceAlpha" result="shadowOffsetOuter1" />
+                    <feGaussianBlur
+                      stdDeviation="1"
+                      in="shadowOffsetOuter1"
+                      result="shadowBlurOuter1"
+                    />
+                    <feColorMatrix
+                      values="0 0 0 0 0.384313725 0 0 0 0 0.878431373 0 0 0 0 0.850980392 0 0 0 0.811141304 0"
+                      in="shadowBlurOuter1"
+                    />
+                  </filter>
+                </defs>
+                <g fill="none" fillRule="evenodd">
+                  <g transform="translate(2 2)">
+                    <use fill="#000" filter="url(#a)" xlinkHref="#b" />
+                    <use
+                      className="group-hover:fill-white"
+                      fill="#62E0D9"
+                      xlinkHref="#b"
+                    />
+                  </g>
+                  <path
+                    d="M8.582 6l-.363.35 1.452 1.4H5.333v.5h4.338L8.22 9.65l.363.35 2.074-2z"
+                    fill="#1B2330"
+                  />
+                </g>
+              </svg>
             </div>
           </div>
-          <hr className="h-px w-full border-[#62E0D9]" />
+          <hr className="h-px w-full border-[#62E0D9] group-hover:border-white" />
         </a>
       </div>
     </section>
