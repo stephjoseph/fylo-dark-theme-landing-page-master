35c95b0d367c3d3449788347f1009832037fa781
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-07 09:55:08 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,19 +14,7 @@ function App() {
       <Main />
       <Footer />
       <div className="z-10">
-        Stay productive, wherever you are Never let location be an issue when
-        accessing your files. Fylo has you covered for all of your file storage
-        needs. Securely share files and folders with friends, family and
-        colleagues for live collaboration. No email attachments required. See
-        how Fylo works Fylo has improved our team productivity by an order of
-        magnitude. Since making the switch our team has become a well-oiled
-        collaboration machine. Satish Patel Founder & CEO, Huddle Fylo has
-        improved our team productivity by an order of magnitude. Since making
-        the switch our team has become a well-oiled collaboration machine. Bruce
-        McKenzie Founder & CEO, Huddle Fylo has improved our team productivity
-        by an order of magnitude. Since making the switch our team has become a
-        well-oiled collaboration machine. Iva Boyd Founder & CEO, Huddle Get
-        early access today It only takes a minute to sign up and our free
+        Get early access today It only takes a minute to sign up and our free
         starter tier is extremely generous. If you have any questions, our
         support team would be happy to help you. Get Started For Free Lorem
         ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
